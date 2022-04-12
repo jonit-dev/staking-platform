b8c7f9f825c7c8fc0b9177e2cb4e0ea20d3549c7
Joao Paulo Furtado <joaopaulofurtado@live.com>
2022-04-12 14:15:20 -0700

    cyborg theme

---
+++ web/pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://jenil.github.io/bulmaswatch/cyborg/bulmaswatch.min.css"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
